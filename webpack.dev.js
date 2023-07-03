2dfeb840c128fb72c98a2b0489dd57041c6be3bf
svvik <svetlana_viktorovna@meta.ua>
2023-07-03 15:16:38 -0700

    Updated readme file; added .env.template file; removed SW generation for dev mode

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,7 +2,6 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const WorkboxPlugin = require("workbox-webpack-plugin");
 
 
 module.exports = {
@@ -45,7 +44,6 @@ module.exports = {
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
         }),
-        // new WorkboxPlugin.GenerateSW(),
         new webpack.HotModuleReplacementPlugin({
             multiStep: true
         })   
